2095f126b48916bea27087e63cd57b569d4806c1
Stella Antonogiannaki <stellaantonogiannaki@Stellas-MacBook-Air.local>
2019-10-17 19:17:17 +0100

    happy dolphin

---
+++ src/stella/dolphin.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Dolphin {
+    public: 
+    int weight;
+    std::string color;
+    double length;
+
+    void life_is_good() {
+        std::cout << "I am a happy dolphin\n";
+    }
+    
+   };
