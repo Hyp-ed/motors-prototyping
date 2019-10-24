5b821609b19174320c729eb6f25671c6c12f26e8
Michal <michal@walls.com.pl>
2019-10-24 19:53:52 +0100

    Add Logger to CanSender

--- src/Michal/CanSender.cpp
+++ src/Michal/CanSender.cpp
@@ -3,13 +3,18 @@
 namespace hyped {
 namespace cansender {
 
-void CanSender::processNewData(Frame& message){
+  CanSender::CanSender(Logger &log, uint8_t node_id): log_(log), node_id_(node_id){
+  }
 
-}
+  void CanSender::processNewData(Frame& message){
 
-bool CanSender::hasId(uint32_t id, bool extended) {
-return true;
-}
+  }
+
+  bool CanSender::hasId(uint32_t id, bool extended) {
+  return id != 0;
+  }
+
+
 
 }
 }
--- src/Michal/CanSender.hpp
+++ src/Michal/CanSender.hpp
@@ -2,23 +2,31 @@
 #define MICHAL_CAN_SENDER_HPP_
 
 #include "utils/io/can.hpp"
-
+#include "utils/logger.hpp"
 
 
 namespace hyped {
 using utils::io::Can;
 using utils::io::can::Frame;
 using utils::io::CanProccesor;
+using utils::Logger;
 
 namespace cansender {
 
 
 class CanSender: public CanProccesor {
 
-void processNewData(Frame& message);
+public:
 
-bool hasId(uint32_t id, bool extended);
+  CanSender(Logger &log, uint8_t node_id);
 
+  void processNewData(Frame& message);
+
+  bool hasId(uint32_t id, bool extended);
+
+private:
+  Logger& log_;
+  uint8_t node_id_;
 
 };
 
