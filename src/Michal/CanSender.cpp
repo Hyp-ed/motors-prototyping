f6b7e4c73172e93dea3652d437aaea96ca5683c0
Michal <michal@walls.com.pl>
2019-10-28 20:50:16 +0000

    Implement processNewData and hasId in CanSender

--- src/Michal/CanSender.cpp
+++ src/Michal/CanSender.cpp
@@ -7,11 +7,26 @@ namespace cansender {
   }
 
   void CanSender::processNewData(Frame& message){
+    uint32_t id = message.id;
+    if(id == node_id_){
 
+          log_.INFO("MOTOR","%0x %0x %0x %0x %0x %0x %0x %0x", message.data[0], message.data[1], message.data[2],message.data[3],
+                     message.data[4], message.data[5], message.data[6], message.data[7]);
+
+        printf("%x %x %x %x %x %x %x %x", message.data[0], message.data[1], message.data[2],message.data[3],
+                     message.data[4], message.data[5], message.data[6], message.data[7]);
+
+    }
+    else {
+        log_.ERR("MOTOR", "Message has wrong id");
+    }
   }
 
   bool CanSender::hasId(uint32_t id, bool extended) {
-  return true;
+    if(id==node_id_){
+        return true;
+    }
+    return false;
   }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "utils/logger.hpp"
 #include "utils/system.hpp"
+#include "Michal/CanSender.hpp"
+#include "utils/io/can.hpp"
 
 using hyped::utils::Logger;
 using hyped::utils::System;
+using hyped::utils::io::can::Frame;
+using hyped::cansender::CanSender;
 
 int main(int argc, char** argv)
 {
@@ -10,4 +14,12 @@ int main(int argc, char** argv)
   Logger log = System::getLogger();
 
   log.INFO("MAIN", "Placeholder main program");
+  CanSender cansender(log, 12);
+
+  Frame message;
+  message.id=12;
+  message.data[0]=10;
+  cansender.processNewData(message);
+
+
 }
