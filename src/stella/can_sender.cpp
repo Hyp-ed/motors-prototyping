0d52a6b0b2f544745c7942708145ff1fc459f9ff
Stella Antonogiannaki <stellaantonogiannaki@c064247.wlan.net.ed.ac.uk>
2019-11-04 20:10:07 +0000

    Complete can_sender files

--- src/stella/can_sender.cpp
+++ src/stella/can_sender.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <atomic>
 #include "utils/io/can.hpp"
 #include "utils/logger.hpp"
 #include "can_sender.hpp"
@@ -7,20 +8,56 @@ using hyped::utils::io::can::Frame;
 using hyped::utils::Logger;
 using hyped::stella::CanSender;
 using hyped::utils::io::CanProccesor;
+using hyped::utils::io::Can;
+
+namespace hyped {
+
+   namespace stella {
 
 CanSender::CanSender(Logger &log, uint8_t node_id)
  :
     log_(log),
-    node_id_(node_id)
+    node_id_(node_id),
+    can_(Can::getInstance())
   {
 
   }
 
 
-void CanSender::processNewData(Frame& message){
+void CanSender::processNewData(Frame &message)
+{
+  isSending = false;
+  uint32_t id = message.id;
+  if (id == node_id_){
+     log_.INFO("MOTOR", "%x",message.data[0], "%x",message.data[1], message.data[2], "%x",message.data[3], "%x",message.data[4], "%x",message.data[5], "%x",message.data[6], "%x",message.data[7]);
+  }
+
+
 
 }
 
-bool hasId(uint32_t id, bool extend){
-   return true; // this is here only to overcome the compiler error, it doesn't do something
+bool CanSender::hasId(uint32_t id, bool extend)
+{
+   if (node_id_ == id) 
+   {
+     return true; 
+   } 
+
+   return false;
 }
+
+bool CanSender::getIsSending()
+{
+  return isSending;
+}
+
+bool CanSender::sendMessage(Frame &message)
+{
+  log_.INFO("MOTOR", "Sending Message");
+  can_.send(message);
+  isSending = true;
+
+  return true;
+}
+
+   }}//namespace hyped::stella
--- src/stella/can_sender.hpp
+++ src/stella/can_sender.hpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include "utils/io/can.hpp"
 #include "utils/logger.hpp"
-
+#include <atomic>
 
 using::hyped::utils::io::can::Frame; 
 using::hyped::utils::Logger;
 using::hyped::utils::io::CanProccesor;
+using::hyped::utils::io::Can;
 
 namespace hyped {
 namespace stella {
 
 
 
-class CanSender : public CanProccesor {
+class CanSender : public CanProccesor
+{
     public:
     void processNewData(Frame& message) override;
     bool hasId(uint32_t id, bool extended) override;
+    bool sendMessage(Frame &message);
+    bool getIsSending();
     CanSender(Logger &log, uint8_t node_id);
     private:
-    Logger log_;
+    Logger& log_;
     uint8_t node_id_;
+    std::atomic<bool> isSending;
+    Can &can_; 
 
 };
 
