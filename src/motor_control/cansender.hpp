320f05aef3f3e9e17c760765b69e4af45754378e
Iain Macpherson <iainmacpherson2@gmail.com>
2019-11-04 19:47:24 +0000

    add cansender class

---
+++ src/motor_control/cansender.hpp
@@ -0,0 +1,48 @@
+#include "utils/io/can.hpp"
+#include "utils/logger.hpp"
+
+
+namespace hyped
+{
+namespace motor_control
+{
+using utils::Logger;
+using utils::io::Can;
+using utils::io::CanProccesor;
+using utils::io::can::Frame;
+
+class CanSender : public CanProccesor
+{
+ public:
+  /**
+   * @brief Construct a new Can Sender object
+   * 
+   * @param log_ 
+   * @param node_id 
+   */
+  CanSender(Logger &log_, uint8_t node_id);
+
+  /**
+   * @brief function to process data received
+   * 
+   * @param message 
+   */
+  void processNewData(Frame &message) override;
+
+  /**
+   * @brief Check that the message is meant for this node
+   * 
+   * @param id 
+   * @param extended 
+   * @return true 
+   */
+  bool hasId(uint32_t id, bool extended) override;
+
+ private:
+  Logger& log_;
+  uint8_t node_id_;
+  Can& can_;
+  
+};
+
+}}  // namespace hyped::motor_control
