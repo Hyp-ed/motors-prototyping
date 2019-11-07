961ea9f3c7069cb4908edb77f483f9d26039bf48
Michal <michal@walls.com.pl>
2019-11-07 19:03:46 +0000

    Add canSender in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include "utils/logger.hpp"
 #include "utils/system.hpp"
+#include "motor_control/cansender.hpp"
+#include "utils/io/can.hpp"
 
 using hyped::utils::Logger;
 using hyped::utils::System;
+using hyped::motor_control::CanSender;
+using hyped::utils::io::can::Frame;
 
 int main(int argc, char** argv)
 {
   System::parseArgs(argc, argv);
   Logger log = System::getLogger();
+
+  CanSender* cansender = new CanSender(log, 1);
+  Frame message;
+  message.id=1;
+  for(int i = 0; i<8; i++) {
+    message.data[i]=0;
+  }
+  cansender->sendMessage(message);
 
   log.INFO("MAIN", "Placeholder main program");
 }
