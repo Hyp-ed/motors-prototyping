02042153fadc84e8ab396ebb0b81d333c5ab4933
Iain Macpherson <iainmacpherson2@gmail.com>
2019-11-07 19:11:02 +0000

    add sleep to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include "utils/system.hpp"
 #include "motor_control/cansender.hpp"
 #include "utils/io/can.hpp"
+#include "utils/concurrent/thread.hpp"
 
 using hyped::utils::Logger;
 using hyped::utils::System;
 using hyped::motor_control::CanSender;
+using hyped::utils::concurrent::Thread;
 using hyped::utils::io::can::Frame;
 
 int main(int argc, char** argv)
@@ -16,10 +18,12 @@ int main(int argc, char** argv)
   CanSender* cansender = new CanSender(log, 1);
   Frame message;
   message.id=1;
+  message.len = 8;
+  
   for(int i = 0; i<8; i++) {
     message.data[i]=0;
   }
   cansender->sendMessage(message);
 
-  log.INFO("MAIN", "Placeholder main program");
+  Thread::sleep(10);
 }
